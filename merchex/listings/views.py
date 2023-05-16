f7012723db27492ccf96aa508d474afc5c90ae14
ghoyap hyacinthe <hyacinthetchoula@gmail.com>
2023-05-16 13:00:17 +0200

    ajout des template pour l'affichage des page html

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,34 +5,18 @@ from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-    <h1> Hello Django</h1>
-    <p> Mes groupes preferes sont: </p>
-    <ul>
-        <li>{bands[0].name}</li>
-        <li>{bands[1].name}</li>
-        <li>{bands[2].name}</li>
-    </ul>
+    return render(request, 'listings/hello.html',
+                  {'bands' : bands})
     
     
-    """)
+
 def about(request):
-    return HttpResponse('<h1>A propos</h1> <p> Nous adorons merch !</p>')
+    return render(request, 'listings/about.html')
 
 def listings(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-    <h1>Les Listings </h1>
-    <p> Les listings disponibles</p>
-    <ul>
-        <li>{listings[0].title}</li>
-        <li>{listings[1].title}</li>
-        <li>{listings[2].title}</li>
-        <li>{listings[3].title}</li>
-        
-    </ul>
-    
-    """)
+    return render(request, 'listings/listings.html',
+                  {'listings' : listings})
 
 def contact(request):
-    return HttpResponse('<h1>Contact-us</h1>')
+    return render(request, 'listings/contact.html')
