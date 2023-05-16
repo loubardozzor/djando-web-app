3e30fc965adb230af3ab4b278e5ad876c541d6bd
ghoyap hyacinthe <hyacinthetchoula@gmail.com>
2023-05-16 11:38:34 +0200

    ajout du model Band e Listing

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,5 +2,9 @@ from django.db import models
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
 
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
+
+
 
 # Create your models here.
