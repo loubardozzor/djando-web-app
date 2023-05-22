1be9d02d2585c7827a9f1a34a18d9b325e9e0319
ghoyap hyacinthe <hyacinthetchoula@gmail.com>
2023-05-22 13:53:17 +0200

    ajout de nouveau modele d'url a urls.py

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,8 +20,11 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:band_id>/', views.band_detail, name='band-detail'),
+    path('listings/', views.listings_list, name='listings-list'),
+    path('listings/<int:listing_id>/', views.listing_detail, name='listing-detail'),
     path('about-us/', views.about),
     path('contact-us/', views.contact),
-    path('listings/', views.listings)
+
 ]
